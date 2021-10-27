4a45b1ec698599e9d61e4c96c68dc04058e1a00f
jchwenger <34098722+jchwenger@users.noreply.github.com>
2021-10-27 16:21:48 +0100

    vegans | pitch

--- _imports/vegans.sh
+++ _imports/vegans.sh
@@ -11,16 +11,16 @@ touch $POST
 
 s=$(cat "$TITLE".txt)
 
-echo '---'                                       >> $POST
-echo 'layout: single'                            >> $POST
-echo 'classes: wide'                             >> $POST
-echo 'title: "Vegans"'                           >> $POST
-echo 'date: 2021-10-27 13:20:34.525753107 +0100' >> $POST
-echo 'permalink: /vegans/'                       >> $POST
-echo 'unlisted: true'                            >> $POST
-echo 'pitch:'                                    >> $POST
-echo '- "Neural variations"'                     >> $POST
-echo '---'                                       >> $POST
-echo ''                                          >> $POST
-echo "$s"                                        >> $POST
-echo ''                                          >> $POST
+echo '---'                                        >> $POST
+echo 'layout: single'                             >> $POST
+echo 'classes: wide'                              >> $POST
+echo 'title: "Vegans"'                            >> $POST
+echo 'date: 2021-10-27 13:20:34.525753107 +0100'  >> $POST
+echo 'permalink: /vegans/'                        >> $POST
+echo 'unlisted: true'                             >> $POST
+echo 'pitch:'                                     >> $POST
+echo '- "Neural variations from a single prompt"' >> $POST
+echo '---'                                        >> $POST
+echo ''                                           >> $POST
+echo "$s"                                         >> $POST
+echo ''                                           >> $POST
